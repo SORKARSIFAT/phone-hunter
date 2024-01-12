bafc8575c40ac45bb1cf1b2ba89e2ae355ceeaa0
SORKARSIFAT <mominulsorkar971@gmail.com>
2024-01-12 23:43:18 +0600

    javascript

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const loadPhones = async()=>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhone(data.data);
+}
+
+const displayPhone = phones =>{
+    console.log(phones);
+
+}
+
+
+
+
+loadPhones()
