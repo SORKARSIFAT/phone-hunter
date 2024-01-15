1431a3dfd3947a322472f517e88670125a6080db
SORKARSIFAT <mominulsorkar971@gmail.com>
2024-01-15 19:38:17 +0600

    add card

--- js/app.js
+++ js/app.js
@@ -6,7 +6,26 @@ const loadPhones = async()=>{
 }
 
 const displayPhone = phones =>{
-    console.log(phones);
+   
+    const phoneContainer = document.getElementById("phone-container");
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneDiv = document.createElement("div");
+        phoneDiv.classList.add("col");
+        phoneDiv.innerHTML=`
+        <div class="card shadow-md bg-body-tertiary rounded">
+        <img class="px-3 py-3 " src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+
+        `
+        phoneContainer.appendChild(phoneDiv);
+
+        
+    });
 
 }
 
